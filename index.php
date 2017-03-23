96ea299ab658c522d7ad3bcdca3d7db2eb768d3f
StoVictor <1vitek99@gmail.com>
2017-03-23 22:08:37 +0200

    Основные изменения модели

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
+<?php
+    require_once '/model/model.php';
+    var_dump(GetNewsName('1'));
+    var_dump(GetNewsPathImg('1'));
+    var_dump(GetNewsPathText('1'));
+    var_dump(GetNewsDate('1'));
+    var_dump(GetNewsId());
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <title>Sto-News</title>
