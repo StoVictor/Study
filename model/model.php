1ab7cb702c9bb7d017de50b648e87cbf452ad997
StoVictor <1vitek99@gmail.com>
2017-03-23 22:10:26 +0200

    Настоящие основные изменения модели

--- model/model.php
+++ model/model.php
@@ -1,8 +1,41 @@
 <?php
+    
     function SqlQueryDo($query){
         mysql_connect('localhost','root','');
         mysql_select_db('News');
-        mysql_query($query);
+        while ($res = mysql_query($query)){
+            $complRes = $res; 
+        }
         mysql_close();
+        return $res;
     }
+
+    function GetSomethingFromNews($id,$whatNeedBring){
+        $que = 'SELECT `'.$whatNeedBring.'` FROM news WHERE `id` ='.$id;
+        $res = SqlQueryDo($que);
+        $arr = mysql_fetch_assoc($res);
+        $something = $arr[$whatNeedBring];
+        return $something;
+    }
+
+    function GetNewsName($id){
+        return GetSomethingFromNews($id,'name');       
+    }
+    function GetNewsPathImg($id){
+        return GetSomethingFromNews($id,'pathToImg');
+    }
+    function GetNewsPathText($id){
+        return GetSomethingFromNews($id,'pathToText');    
+    }
+    function GetNewsDate($id){
+        return GetSomethingFromNews($id,'date');    
+    }
+    
+    function GetNewsId(){
+        $que = 'SELECT `id` FROM `news`';
+        $arr = SqlQueryDo($que);
+        $arr = mysql_fetch_assoc($arr);
+        return $arr;
+    }
+
 ?>
