6a7ac439bd4b8197f06e32cee71a3215c424b4a0
StoVictor <1vitek99@gmail.com>
2017-03-28 23:19:34 +0300

    Начально готовый index

--- index.php
+++ index.php
@@ -1,18 +1,57 @@
 <?php
-    require_once '/model/model.php';
-    var_dump(GetNewsName('1'));
-    var_dump(GetNewsPathImg('1'));
-    var_dump(GetNewsPathText('1'));
-    var_dump(GetNewsDate('1'));
-    var_dump(GetNewsId());
+    require_once '/view/view.php';
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <title>Sto-News</title>
+    <style>
+        body{
+            background-color: #65b0f0;
+        }
+        .news{
+            background-color: aliceblue;
+            margin: auto;
+            text-align: center;
+            width: 600px;
+            margin-bottom: 20px;
+        }
+        .news h3{
+            padding: 0;
+            margin: 0;
+        }
+        .news img{
+            
+            padding: 0px;
+        }
+        .news p{
+            color: gray;
+            
+        }
+        .news span {
+            display: block;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
 </head>
 <body>
-    
+   <?php
+    $numOfNews = GetNewsOrderedId();
+    foreach($numOfNews as $id): 
+    $arr = GetNews($id);
+    ?>
+   
+    <div class = 'news'>
+        <h3><a><?php echo $arr[0]['name'] ?></a></h3>
+        <span> <?php echo $arr[0]['date']?></span>
+        <img src=<?php echo $arr[0]['pathToImg']?>>
+        <p>
+            <?php echo (writePreview($arr[0]['pathToText']))?>
+        </p>
+    </div>
+    <?php endforeach;?>
 </body>
 </html>
